39d201f92e7c9e225fbcfec4893c619704da1a88
Sudhir Vishwakarma <sudhirvi@google.com>
2024-11-29 12:31:41 +0530

    intial data

---
+++ app/serializers.py
@@ -0,0 +1,8 @@
+from .models import *
+from rest_framework import serializers
+
+class EmployeeSerializer(serializers.Serializer):
+    name=serializers.CharField(max_length=30)
+    email=serializers.EmailField()
+    password=serializers.CharField(max_length=30)
+    phone=serializers.CharField(max_length=10)
